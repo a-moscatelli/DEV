f76d3a0b8b8daa54bfdbf5896a168d2d38d693f4
a-moscatelli <a.moscatelli@gmail.com>
2025-04-09 17:17:08 +0200

    Update aws-ec2-ubuntu-setup-new-ec2--Apr2025.sh

--- aws-ec2-ubuntu-setup-new-ec2--Apr2025.sh
+++ aws-ec2-ubuntu-setup-new-ec2--Apr2025.sh
@@ -10,16 +10,12 @@ then
         sudo apt-get update
         echo step 1 - end
 fi
-
-
 if [ 2 -eq 2 ]
 then
         echo step 2 - begin - install and start the docker service
         sudo apt-get install docker.io -y
         echo step 2 - end
 fi
-
-
 if [ 3 -eq 3 ]
 then
         echo step 3 - begin - docker test run: we generate a mini awk program and we run it using the awk inside busybox
@@ -28,3 +24,12 @@ then
         sudo docker run -v /tmp:/tmp999 busybox awk -f /tmp999/minitest.awk
         echo step 3 - end
 fi
+if [ 4 -eq 4 ]
+then
+        echo step 4 - begin - install python pip and then aws cli
+        sudo apt-get -y install python3-pip
+        # ko: python3 -m pip install awscliv2
+        # ko: sudo apt install python-awscliv2
+fi
+        
+
