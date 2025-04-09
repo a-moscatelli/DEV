73fc3c1b62b6f663eadc01adcf601d70e250bf92
a-moscatelli <a.moscatelli@gmail.com>
2025-04-09 16:43:30 +0200

    Add files via upload

---
+++ aws-ec2-ubuntu-setup-new-ec2--Apr2025.sh
@@ -0,0 +1,30 @@
+OS_ACTUAL=$(uname)-$(whoami)
+OS_TOBE=Linux-ubuntu
+echo OS_ACTUAL is $OS_ACTUAL and should be $OS_TOBE
+[ "$OS_ACTUAL" != "$OS_TOBE" ] && exit 105
+echo good - OS_ACTUAL is as expected
+
+if [ 1 -eq 1 ]
+then
+        echo step 1 - begin - update all packages
+        sudo apt-get update
+        echo step 1 - end
+fi
+
+
+if [ 2 -eq 2 ]
+then
+        echo step 2 - begin - install and start the docker service
+        sudo apt-get install docker.io -y
+        echo step 2 - end
+fi
+
+
+if [ 3 -eq 3 ]
+then
+        echo step 3 - begin - docker test run: we generate a mini awk program and we run it using the awk inside busybox
+        AWKZ=/tmp/minitest.awk
+        echo 'BEGIN{print "hi from awk"}' > $AWKZ
+        sudo docker run -v /tmp:/tmp999 busybox awk -f /tmp999/minitest.awk
+        echo step 3 - end
+fi
